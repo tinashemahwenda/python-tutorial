0df8097786c7aed0afc4263b0bd480cc7312000b
tinashemahwenda <tinashemahwenda@gmail.com>
2021-03-22 15:54:58 +0200

    update loops_me.py

---
+++ loops_me.py
@@ -0,0 +1,17 @@
+#while loop
+#controller
+'''x = 0
+names = []
+while x<5:
+    name = input('Enter a name : ')
+    names.append(name)
+    x=x+1
+print(names)'''
+
+i = 0
+while i <= 10:
+    print(i)
+    i=i+3
+    
+
+  
